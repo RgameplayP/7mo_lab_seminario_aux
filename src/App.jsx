128b9dfa6ed67ca0020ab8b21f562e212b38818d
Aldrin Roger Perez <rogerperez1309@gmail.com>
2024-04-12 17:51:52 -0400

    feature(common): [added Question-2]

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { Button } from './components/Button'
-import { WapperList } from './components/WrapperLis'
-import { List } from './components/List'
 
 function App() {
 
@@ -12,13 +7,7 @@ function App() {
 
   return (
     <>
-
-      <WapperList>
-        
-       <List/>
-
-      </WapperList>
-    
+    <Button/>
     </>
   )
 }
